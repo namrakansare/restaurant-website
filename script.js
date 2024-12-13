0e2d14c2fb4a6aac75f8810b059a8bcc654ca8df
namrakansare <namrakansare@gmail.com>
2024-12-13 17:14:46 -0500

    First Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const menuOpenButton =document.getElementById("#menu-open-button");
+const menuCloseButton =document.getElementById("#menu-close-button");
+menuOpenButton.addEventListener("click", () =>{
+    document.body.classList.toggle("show-mobile-menu");
+});
+menuCloseButton.addEventListener("click", () =>{
+    
+});
